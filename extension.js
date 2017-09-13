850626b474f0678893c79616787ead2bd7b5f2f1
Dan Shahin <d.shahin@dshahin-ltm.internal.salesforce.com>
2017-09-13 15:03:19 -0700

    Integrate with public lightning linter service

--- extension.js
+++ extension.js
@@ -3,6 +3,23 @@
 var vscode = require('vscode');
 var linterService = require('./linterService');
 
+const linterDecorationType = vscode.window.createTextEditorDecorationType({
+//    isWholeLine: true,
+    borderWidth: '2px',
+    borderStyle: 'solid',
+    color: 'red',
+    overviewRulerColor: 'red',
+    overviewRulerLane: vscode.OverviewRulerLane.Right,
+    light: {
+        // this color will be used in light color themes
+        borderColor: 'red'
+    },
+    dark: {
+        // this color will be used in dark color themes
+        borderColor: 'red'
+    }
+});
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
@@ -18,11 +35,40 @@ function activate(context) {
         // The code you place here will be executed every time your command is executed
 
         // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!' + linterService.hello());
+        var editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            return; // No open text editor
+        }
+
+        var text = editor.document.getText();
+
+        //vscode.window.showInformationMessage('Hello World!' + text);
+        linterService.lintFile(text).then(function(results){
+            var lintResults = JSON.parse(results),
+                decorations = [];
+            for(var i=0; i<lintResults.length;i++){
+                var lintResult = lintResults[i];
+                decorations.push(decorate(lintResult) );
+            }
+            editor.setDecorations(linterDecorationType,decorations);
+            
+        })
     });
 
     context.subscriptions.push(disposable);
 }
+
+function decorate(lintResult){
+    console.log(lintResult);
+    var line = lintResult.line-1
+    var startPos = new vscode.Position(line, lintResult.column - 1);
+    var endPos = new vscode.Position(line,lintResult.column );
+    var decoration = { range: new vscode.Range(startPos, endPos), hoverMessage: lintResult.message };
+    return decoration;
+    
+}
+
+
 exports.activate = activate;
 
 // this method is called when your extension is deactivated
--- linterService.js
+++ linterService.js
@@ -1,4 +1,55 @@
-// mymodule.js
+// linterService.js
+var http = require('http');
 exports.hello = function() {
     return "Hello";
 }
+
+exports.lintFile = function(content){
+    var headers = {
+        'Content-Type': 'text/plain',
+        'Origin': 'https://lightning-linter.herokuapp.com/lint_code',
+        'Accept': 'text/plain, */*; q=0.01',
+        'Connection': 'keep-alive'
+    }
+    var options = {
+        host: 'lightning-linter.herokuapp.com',
+        path: '/lint-json',
+        method: 'POST',
+        headers: headers
+    };
+    return httpRequest(options,content);
+}
+
+// Raw htpp post request wrapped in promise.
+function httpRequest(options, content) {
+    return new Promise(function(resolve, reject) {
+        var req = http.request(options, function(res) {
+            // reject on bad status
+            if (res.statusCode < 200 || res.statusCode >= 300) {
+                return reject(new Error('statusCode=' + res.statusCode));
+            }
+            // cumulate data
+            //var body = [];
+            var str = '';
+            res.on('data', function(chunk) {
+                str += chunk;
+            });
+            // resolve on end
+            res.on('end', function() {
+                try {} catch (e) {
+                    reject(e);
+                }
+                resolve(str);
+            });
+        });
+        req.on('error', function(err) {
+            // This is not a "Second reject", just a different sort of failure
+            reject(err);
+        });
+        if (content) {
+            req.write(content);
+        }
+        // IMPORTANT
+        req.end();
+    });
+};
